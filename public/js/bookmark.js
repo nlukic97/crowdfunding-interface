6ef67ce8739f211f9cc75bf63d7e305c81679546
nlukic97 <nlukic97@gmail.com>
2021-10-19 11:52:08 +0200

    hamburger menu made

--- public/js/bookmark.js
+++ public/js/bookmark.js
@@ -1,3 +1,10 @@
+// Bookmark button events
 document.querySelector('.bookmark-button').addEventListener('click',function(){
     this.classList.toggle('bookmarked')
+})
+
+
+// hamburger icon events
+document.querySelector('#hamburger-icon').addEventListener('click',function(){
+    document.body.classList.toggle('hamburger-open')
 })
