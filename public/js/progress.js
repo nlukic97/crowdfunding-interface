513156f585577dbbb7f5b10bbe028e96ced9c058
nlukic97 <nlukic97@gmail.com>
2021-10-19 15:33:09 +0200

    finished progress, started about section

--- public/js/progress.js
+++ public/js/progress.js
@@ -2,9 +2,8 @@
 let gathered = parseInt(document.querySelector('#gathered').innerText.replaceAll(',',''))
 let goal = parseInt(document.querySelector('#goal').innerText.replaceAll(',',''))
 
-console.log(gathered,goal);
 
 let percentage = Math.floor(gathered/ goal * 100)
-console.log(percentage);
+
 
 document.querySelector('.progress-bar').style='width:' + percentage + '%';
