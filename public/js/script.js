e4f6e3aaf99af2a9d8bea8ff58b105e9b0eda93e
nlukic97 <nlukic97@gmail.com>
2021-10-20 12:52:44 +0200

    added listener for btn to back the project

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+//adding listener for click on 'back this project' button to display the modal and overlay
+document.getElementById('back-project-btn').addEventListener('click',function(){
+    console.log('bookmark buttom clicked');
+})
